50d72f0130eaadda075d0d9b6971d6f1b34e04a6
marmamadou233-lab <marmamadou233@gmail.com>
2026-09-17 13:42:22 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = "dahiratoul-imani-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./accueil.html"
+];
+
+self.addEventListener("install", function (event) {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+
+                })
+            );
+
+        })
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+    event.respondWith(
+
+        fetch(event.request)
+            .catch(function () {
+
+                return caches.match(event.request);
+
+            })
+
+    );
+
+});
